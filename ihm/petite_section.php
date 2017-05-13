0c4505fcab91ae7641263d8b279d9786ede18923
beamishStout <louis.aneiros@gmail.com>
2017-05-13 23:41:08 +0200

    Merge origin/master
    Readme : description de la procédure d'install
    petite_section : commencement de la page standard des classe

---
+++ ihm/petite_section.php
@@ -0,0 +1,28 @@
+<?php
+include_once("header.php");?>
+
+<div id="section">
+     <h1><span class="lettre-bleu">P</span>etite <span class="lettre-orange">s</span>ection</h1>
+</div>
+<div class="maitresse">
+    
+</div>
+<div class="container">
+    <div class="ligne ligne-rouge">
+        <div class="left texte">
+            <p><?php echo nl2br($lignes[0]);?></p>
+        </div>
+        <div class="right">
+             <img src="donnees/img/<?php echo $images[0]?>" alt="" />
+        </div>
+    </div>
+    <div class="ligne ligne-bleu">
+        <div class="left">
+            <img src="donnees/img/<?php echo $images[1]?>" alt="" />
+        </div>
+        <div class="right texte">
+            <p><?php echo nl2br($lignes[1]);?></p>
+        </div>
+    </div>
+</div>
+<?php include_once("footer.php");?>
