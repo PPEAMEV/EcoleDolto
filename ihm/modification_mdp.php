01c355b68ab77fa0c6a219fa9b42a7ea88d8f604
beamishStout <louis.aneiros@gmail.com>
2017-06-08 16:42:33 +0200

    ihm modification mdp

--- ihm/modification_mdp.php
+++ ihm/modification_mdp.php
@@ -1,4 +1,4 @@
-<form method="post" action="" onsubmit="return verification()">
+<form id="formConnexion" method="post" action="" onsubmit="return verification()">
     Nouveau mot de passe : <input type="password" name="nmdp" id="nouveau1">
     Verification mot de passe : <input type="password" name="vmdp" id="nouveau2">
     <input type="submit" name="submit" value=" Envoyer ">
